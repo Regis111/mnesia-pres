d13d961dde1ed56b6ee5c1fce0bebae016e2e34d
regis <jerzy.wenta@protonmail.com>
2019-05-30 18:41:24 +0200

    repaired removing stuff + finished adding points

--- labs.erl
+++ labs.erl
@@ -1,25 +1,31 @@
 -module(labs).
 
--export([init/1, add_lab/2, add_student/3, remove_student/1, remove_lab/1]).
+-export([
+         init/1, delete/1, add_lab/2, add_student/3, 
+         remove_student/1, remove_lab/1, add_point_to_student/2
+        ]).
 
--record(labs, {date, teacher}).
--record(students, {firstname, surname, lab_date, points = []}).
+-record(laboratories, {date, teacher}).
+-record(students, {surname, firstname, lab_date, points = []}).
+
+delete(Nodes) -> mnesia:delete_table(laboratories),
+                 mnesia:delete_table(students),
+                 mnesia:stop(),
+                 mnesia:delete_schema(Nodes).
 
 init(Nodes) -> 
     ok = mnesia:create_schema(Nodes),
     rpc:multicall(Nodes, mnesia, start, []),  
-    mnesia:create_table(labs, 
-                        [{attributes, record_info(fields, labs)},
-                        {disc_copies, Nodes},
-                        {index, [#labs.date]}]),
-    mnesia:create_table(students, 
+    {atomic, ok} = mnesia:create_table(laboratories, 
+                        [{attributes, record_info(fields, laboratories)},
+                        {disc_copies, Nodes}]),
+    {atomic, ok} = mnesia:create_table(students, 
                         [{attributes, record_info(fields, students)},
-                        {disc_copies, Nodes},
-                        {index, [#students.surname]}]),
-    rpc:multicall(Nodes, application, stop, []).
+                        {disc_copies, Nodes}
+                        ]).
 
 add_lab(Date, Teacher) ->
-    Lab = #labs{
+    Lab = #laboratories{
              date = Date, 
              teacher = Teacher
             },
@@ -33,16 +39,16 @@ add_student(FirstName, Surname, Date) ->
                  surname = Surname, 
                  lab_date = Date
                 },
-    F = fun() -> case mnesia:index_read(labs, Date, date) of
+    F = fun() -> case mnesia:read({laboratories, Date}) of
                     []      -> {error, no_such_lab};
-                    [Lab]   -> mnesia:write(Student)
+                    [_]     -> mnesia:write(Student)
                  end 
         end,
     {atomic, Result} = mnesia:transaction(F),
     Result.
 
 remove_student(Surname) -> 
-    F = fun() -> case mnesia:index_read(students, Surname, surname) of
+    F = fun() -> case mnesia:read({students, Surname}) of
                      []         -> {error, no_such_student};
                      [Student]  -> mnesia:delete({students, Student#students.surname})
                  end
@@ -51,16 +57,30 @@ remove_student(Surname) ->
     Result.
 
 remove_lab(Date) ->
-    F = fun() -> case mnesia:index_read(labs, Date, date) of
+    F = fun() -> case mnesia:read({laboratories, Date}) of
                      []         -> {error, no_such_lab};
-                     [Lab]      -> mnesia:delete({labs, Lab#labs.date})
+                     [Lab]      -> mnesia:delete({laboratories, Lab#laboratories.date})
                  end
         end,
     {atomic, Result} = mnesia:transaction(F),
     Result.
 
 add_point_to_student(Value, Surname) ->
-    F = fun() -> case mnesia:index_read(students, Surname, surname) of
+    F = fun() -> case mnesia:read({students, Surname}) of
                      []         -> {error, no_such_student};
-                     [Student]  -> mnesia:write( students#Student{points
+                     [Student]  -> mnesia:delete({students, Surname}),
+                                   mnesia:write(Student#students{points = [Value | Student#students.points]})
+                 end
+        end,
+    {atomic, Result} = mnesia:transaction(F),
+    Result. 
 
+%TO DO
+%
+%select labs - CZAJKA ZA CO CI PŁACE!!!
+%
+%select students - CZAJKA ZA CO CI PŁACE!!!
+%
+%add observer node
+%
+%remove observer node
